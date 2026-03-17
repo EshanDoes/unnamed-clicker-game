6e7cca9545c50050363f69352e962fd454723065
EshanDoes <eshan.shiraz@gmail.com>
2026-03-17 13:07:46 -0600

    Added shop and other stuff
    - Added a shop with one item in it so far that adds one to the number of clicks you have
    - Adjusted animations for compatibility

--- scripts/gameManager.gd
+++ scripts/gameManager.gd
@@ -3,12 +3,18 @@ extends Node
 @onready var button = $"Monitor Button"
 @onready var clicksCounter = $"Click Counter"
 @onready var timer = $Timer
+@onready var shop = $Shop
+@onready var shopItems = $Shop/Items.get_children()
+@export var itemCosts: Array[int] = []
 var buttonSize = 64
 var counterPosition = 0
+var shopPosition = 160
+
 var time = 0.0
 var clicks = 0
 @export var clickIncrement = 1.0
 @export var autoClicksPerSecond = 0.0
+var shopOpen = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,14 +25,37 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	button.rotation = sin(time) / 10
 	time += delta
-	
+
+
+# Animations for every animated node
+func _physics_process(delta: float) -> void:
 	button.size = Vector2(buttonSize, buttonSize)
 	button.position = Vector2(100-buttonSize/2, 100-buttonSize/2)
-	if buttonSize > 64:
-		buttonSize -= delta*10*(buttonSize-64)
+	if buttonSize > 64.1: buttonSize -= delta*10*(buttonSize-64)
+	else: buttonSize = 64
+	
 	clicksCounter.position.y = counterPosition
-	if counterPosition > 0:
-		counterPosition -= delta*5*counterPosition
+	if counterPosition > 0.1: counterPosition -= delta*5*counterPosition
+	else: counterPosition = 0
+	
+	shop.position.y = shopPosition
+	clicksCounter.position.x = (160-shopPosition)*-0.25
+	if shopOpen and shopPosition > 0.1: shopPosition -= delta*15*shopPosition
+	elif shopOpen: shopPosition = 0
+	if !shopOpen and shopPosition < 159.9: shopPosition += delta*15*(160-shopPosition)
+	elif !shopOpen: shopPosition = 160
+	
+	for item in shopItems:
+		var icon = item.get_node("Icon")
+		var iconSize = icon.size.x
+		if iconSize > 32.1: iconSize -= delta*10*(iconSize-32)
+		else: iconSize = 32
+		icon.size = Vector2(iconSize, iconSize)
+		icon.position = Vector2(14-(iconSize-32)/2, 8-(iconSize-32)/2)
+		var iconRotation = icon.rotation
+		if iconRotation > 0.01:
+			icon.rotation -= delta*5*iconRotation
+		else: iconRotation = 0
 
 
 func _on_tv_clicked(event: InputEvent) -> void:
@@ -49,6 +78,24 @@ func add_clicks(amount = autoClicksPerSecond):
 	else:
 		clickDisplay = str(clicks)
 	
-	if clicksCounter.text != clickDisplay:
+	if amount != 0:
 		clicksCounter.text = clickDisplay
 		counterPosition = 4
+
+
+func _shop_button_pressed(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if shopOpen: shopOpen = false
+		else: shopOpen = true
+
+
+func _click_item_bought(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if itemCosts[0] <= clicks:
+			clickIncrement += 1
+			shopItems[0].get_node("Icon").size = Vector2(44, 44)
+			add_clicks(itemCosts[0]*-1)
+			itemCosts[0] = itemCosts[0] * 1.05
+			shopItems[0].get_node("Cost").text = "Cost: " + str(itemCosts[0])
+		else:
+			shopItems[0].get_node("Icon").rotation = 0.2
